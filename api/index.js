c935cc5949ccb9d9d6427d69be2979c440a6927d
hbin1911 <rajas.mulik@gmail.com>
2023-12-15 20:11:36 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3001, ()=>{
-    console.log("Server is running on port 3001");
-})
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
