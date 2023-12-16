34e2377b343cc7acebc9ab164f085187d4a8d50b
hbin1911 <rajas.mulik@gmail.com>
2023-12-16 15:58:41 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 // import userRouter from "./routes/user"
 import dotenv from "dotenv";
 import  userRouter  from "./routes/user.js";
+import authRouter from "./routes/auth.js";
 dotenv.config();
 
 mongoose
@@ -15,10 +16,12 @@ mongoose
   });
 
 const app = express();
+app.use(express.json())
 
 
 app.listen(3007, () => {
     console.log("Server is running on port 3007");
 });
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
